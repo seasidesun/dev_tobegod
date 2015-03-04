943a3c12f381a72178d35f53698c8c979733c965
sunhaibin <sunhaibin@goyoo.com>
2015-03-04 19:41:09 +0800

    add home option:plan

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 
 var errorHandler = require('../lib/utils/error_handler');
 var pandoraRounter = require('../lib/v1/pandora/router');
+var planRounter = require('../lib/v1/plan/router');
 var userRounter = require('../lib/v1/user/router');
 
 module.exports = function (app) {
@@ -10,6 +11,7 @@ module.exports = function (app) {
     app.get('/ping', function (req, res, next) { res.send("ok"); });
 
     app.use('/pandora', pandoraRounter);
+    app.use('/plan', planRounter);
     app.use('/user', userRounter);
 
     // 404 handler
